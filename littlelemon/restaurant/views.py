6ce1bc0b019a2521cc8792e0b53fd26500c90d13
Devs7026 <d2twins2607@gmail.com>
2025-02-02 20:24:03 +0530

    set up serializers

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 # def index(request):
@@ -6,3 +9,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+# Handles GET (list all items) and POST (create new item)
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Handles GET (retrieve single item), PUT (update item), and DELETE (remove item)
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    # Specify the model for the viewset
+    queryset = Booking.objects.all()  # Fetch all booking objects
+    serializer_class = BookingSerializer  
